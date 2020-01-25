ab2c2c78be07214cd6ef137a07fbd95ec7fcb569
Napasin Hongngern <napasin_h@Napasins-MacBook-Pro.local>
2020-01-25 19:31:48 +0700

    Create PAFPN, PANET model

--- mmdetection/mmdet/models/necks/__init__.py
+++ mmdetection/mmdet/models/necks/__init__.py
@@ -2,5 +2,6 @@ from .bfp import BFP
 from .fpn import FPN
 from .hrfpn import HRFPN
 from .nas_fpn import NASFPN
+from .pafpn import PAFPN
 
-__all__ = ['FPN', 'BFP', 'HRFPN', 'NASFPN']
+__all__ = ['FPN', 'BFP', 'HRFPN', 'NASFPN', 'PAFPN']
--- mmdetection/mmdet/models/necks/pafpn.py
+++ mmdetection/mmdet/models/necks/pafpn.py
@@ -1,15 +1,16 @@
-import torch.nn as nn
-import torch.nn.functional as F
-from mmcv.cnn import xavier_init
-
-from mmdet.core import auto_fp16
 from ..registry import NECKS
-from ..utils import ConvModule
 
+@NECKS.register
+class PAFPN(nn.Module):
 
-@NECKS.register_module
-class FPN(nn.Module):
     def __init__(self,
+                in_channels,
+                out_channels,
+                num_outs,
+                start_level=0,
+                end_level=-1,
+                add_extra_convs=False):
+        def __init__(self,
                  in_channels,
                  out_channels,
                  num_outs,
@@ -22,7 +23,7 @@ class FPN(nn.Module):
                  conv_cfg=None,
                  norm_cfg=None,
                  activation=None):
-        super(FPN, self).__init__()
+        super(RESFPN, self).__init__()
         assert isinstance(in_channels, list)
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -48,8 +49,10 @@ class FPN(nn.Module):
 
         self.lateral_convs = nn.ModuleList()
         self.fpn_convs = nn.ModuleList()
-        #self.bottomup_convs = nn.ModuleList()
-        for i in range(self.start_level, self.backbone_end_level):
+        self.downup_sampling=nn.ModuleList()
+        self.res_convs=nn.ModuleList()
+
+         for i in range(self.start_level, self.backbone_end_level):
             l_conv = ConvModule(
                 in_channels[i],
                 out_channels,
@@ -58,6 +61,10 @@ class FPN(nn.Module):
                 norm_cfg=norm_cfg if not self.no_norm_on_lateral else None,
                 activation=self.activation,
                 inplace=False)
+            self.lateral_convs.append(l_conv)
+
+        for i in range(self.start_level, self.backbone_end_level+1):
+
             fpn_conv = ConvModule(
                 out_channels,
                 out_channels,
@@ -67,29 +74,28 @@ class FPN(nn.Module):
                 norm_cfg=norm_cfg,
                 activation=self.activation,
                 inplace=False)
-
-            self.lateral_convs.append(l_conv)
             self.fpn_convs.append(fpn_conv)
-            
-        # add extra conv layers (e.g., RetinaNet)
-        extra_levels = num_outs - self.backbone_end_level + self.start_level
-        if add_extra_convs and extra_levels >= 1:
-            for i in range(extra_levels):
-                if i == 0 and self.extra_convs_on_inputs:
-                    in_channels = self.in_channels[self.backbone_end_level - 1]
-                else:
-                    in_channels = out_channels
-                extra_fpn_conv = ConvModule(
-                    in_channels,
-                    out_channels,
-                    3,
-                    stride=2,
-                    padding=1,
-                    conv_cfg=conv_cfg,
-                    norm_cfg=norm_cfg,
-                    activation=self.activation,
-                    inplace=False)
-                self.fpn_convs.append(extra_fpn_conv)
+
+        for i in range(self.start_level,self.backbone_end_level):
+            down_sampling=nn.MaxPool2d(2,stride=2)
+
+            self.downup_sampling.append(down_sampling)
+        for i in range(self.start_level,self.backbone_end_level):
+            res_conv = ConvModule(
+                in_channels[i],
+                out_channels,
+                1,
+                conv_cfg=conv_cfg,
+                norm_cfg=norm_cfg if not self.no_norm_on_lateral else None,
+                activation=self.activation,
+                inplace=False)
+            self.res_convs.append(res_conv)
+        self.conv_p6=ConvModule(in_channels[-1],out_channels,3,stride=2,padding=1,
+                                conv_cfg=conv_cfg,
+                                norm_cfg=norm_cfg if not self.no_norm_on_lateral else None,
+                                activation=self.activation,
+                                inplace=False
+                                )
 
     # default init_weights for conv(msra) and norm in ConvModule
     def init_weights(self):
@@ -106,10 +112,16 @@ class FPN(nn.Module):
             lateral_conv(inputs[i + self.start_level])
             for i, lateral_conv in enumerate(self.lateral_convs)
         ]
+        #self.lateral_conv
+        p6=self.conv_p6(inputs[-1])
+        p6_down=F.interpolate(p6,scale_factor=2,mode='nearest')
 
         # build top-down path
         used_backbone_levels = len(laterals)
+        laterals[used_backbone_levels-1]+=p6_down
+
         for i in range(used_backbone_levels - 1, 0, -1):
+
             laterals[i - 1] += F.interpolate(
                 laterals[i], scale_factor=2, mode='nearest')
 
@@ -118,24 +130,28 @@ class FPN(nn.Module):
         outs = [
             self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)
         ]
+        outs.append(p6)
 
-        # part 2: add extra levels
-        if self.num_outs > len(outs):
-            # use max pool to get more levels on top of outputs
-            # (e.g., Faster R-CNN, Mask R-CNN)
-            if not self.add_extra_convs:
-                for i in range(self.num_outs - used_backbone_levels):
-                    outs.append(F.max_pool2d(outs[-1], 1, stride=2))
-            # add conv layers on top of original feature maps (RetinaNet)
+
+        N=[]
+        for i in range(used_backbone_levels+1):
+            if i==0:
+                N.append(outs[i])
             else:
-                if self.extra_convs_on_inputs:
-                    orig = inputs[self.backbone_end_level - 1]
-                    outs.append(self.fpn_convs[used_backbone_levels](orig))
-                else:
-                    outs.append(self.fpn_convs[used_backbone_levels](outs[-1]))
-                for i in range(used_backbone_levels + 1, self.num_outs):
-                    if self.relu_before_extra_convs:
-                        outs.append(self.fpn_convs[i](F.relu(outs[-1])))
-                    else:
-                        outs.append(self.fpn_convs[i](outs[-1]))
+                N.append(self.downup_sampling[i-1](N[i-1])+outs[i])
+                # N.append(F.interpolate(N[i-1],scale_factor=0.5,mode='nearest')+outs[i])
+
+
+        res_out=[]
+        for i in range(used_backbone_levels):
+            res_out.append(N[i]+self.res_convs[i](inputs[i]))
+        res_out.append(N[-1]+p6)
+
+        outs = [
+            self.fpn_convs[i](res_out[i]) for i in range(used_backbone_levels+1)
+        ]
+        # if self.num_outs>len(res_out):
+        #     if not self.add_extra_convs:
+        #         for i in range(self.num_outs-used_backbone_levels):
+        #             outs2.append(F.max_pool2d(outs[-1],1,stride=2))
         return tuple(outs)
